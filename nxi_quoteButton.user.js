e65e1a254945c2cf91336391ce99474ca383db3d
arcy-nxi <31899834+arcy-nxi@users.noreply.github.com>
2017-09-12 21:38:19 +0200

    MàJ en 1.0.1
    Évènement "scroll" - corrige le cas où les commentaires ne sont pas chargés au passage du script

--- nxi_quoteButton.user.js
+++ nxi_quoteButton.user.js
@@ -3,29 +3,41 @@
 // @namespace   https://www.nextinpact.com/
 // @match       https://www.nextinpact.com/*
 // @description Ajoute le bouton de citations dans la barre d'action
-// @version     1.0
+// @version     1.0.1
 // @grant       Arcy / arcy-nxi
 // ==/UserScript==
-console.log("Script NXI - citations chargé");
 
+console.log("Script NXI - citations 1.0.1 chargé");
+
+var isLoaded = false;
+
+// évenement "load" -> si le navigateur charge l'article ET les commentaires en même temps (avant l'exécution de ce script)
+// évènement "onscroll" -> si le navigateur charge le script avant les commentaires
 if(window.addEventListener){
     window.addEventListener('load', createButton, false);
+    window.addEventListener('scroll', createButton, false);
 }
 else{
     window.attachEvent('onload', createButton);
+    window.attachEvent('onscroll', createButton);
 }
 
 function createButton(){
-    var $quoteAction = document.createElement("button");
-    $quoteAction.id = "quote_action";
-    $quoteAction.title = "Citations";
-    $quoteAction.classList.add("icon-quote");
-    $quoteAction.classList.add("btn-action-editor"); 
-    $quoteAction.innerHTML = "Q";
-    $quoteAction.onclick = eventClickButton
-    document.getElementById("action_button_post_comment").appendChild($quoteAction);
+    if (document.getElementById("global_comment") != null && !isLoaded){
+        isLoaded = true;
+        
+        var $quoteAction = document.createElement("button");
+        $quoteAction.id = "quote_action";
+        $quoteAction.title = "Citations";
+        $quoteAction.classList.add("icon-quote");
+        $quoteAction.classList.add("btn-action-editor"); 
+        $quoteAction.innerHTML = "Q";
+        $quoteAction.onclick = eventClickButton;
+        
+        document.getElementById("action_button_post_comment").appendChild($quoteAction);
+    }
 }
 
 function eventClickButton(){ 
-   document.getElementById("textarea_comment").value += "[quote][/quote]"
+   document.getElementById("textarea_comment").value += "[quote][/quote]";
 }
